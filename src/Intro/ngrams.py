9761493a226c414dac9289c9e7931c6d06736ff5
Damir Cavar <dcavar@me.com>
2013-06-25 17:05:42 -0400

    some new code on document models

--- src/Intro/ngrams.py
+++ src/Intro/ngrams.py
@@ -7,10 +7,9 @@ Function to generate an N-gram model from a sequence (string, list...).
 """
 
 
-def makeNgramModel(tokenlist, n):
+def makeNgramModel(tokenlist, n, fp={}):
    """This function generates an N-gram model as a dictionary data-structure.
    """
-   fp = {}
    for start in range( len(tokenlist) - (n - 1) ):
       tokenslice = tokenlist[start : start + n]
       #print("Token-slice:", tokenslice)
@@ -22,5 +21,6 @@ def makeNgramModel(tokenlist, n):
       fp[stringngram] = fp.get(stringngram, 0) + 1
    return fp
 
-#print(makeNgramModel("this is a small test", 3))
+if __name__ == '__main__':
+   print(makeNgramModel("this is a small test".split(), 3))
 
